3c29d11fc6ca33fe6ca710bab55e8b3d416a2496
TheDeepIsle <smithd6240@gmail.com>
2026-06-27 22:11:37 -0400

    v1.0.46

--- preload.js
+++ preload.js
@@ -2,8 +2,8 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
     openSteamAuth: (url) => ipcRenderer.invoke('open-steam-auth', url),
-    onPTTDown: (cb) => ipcRenderer.on('ptt-down', cb),
-    onPTTUp: (cb) => ipcRenderer.on('ptt-up', cb),
+    onPTTDown: (cb) => { ipcRenderer.removeAllListeners('ptt-down'); ipcRenderer.on('ptt-down', cb); },
+    onPTTUp: (cb) => { ipcRenderer.removeAllListeners('ptt-up'); ipcRenderer.on('ptt-up', cb); },
     onUpdateAvailable: (cb) => ipcRenderer.on('update-available', cb),
     onUpdateDownloaded: (cb) => ipcRenderer.on('update-downloaded', cb),
 });
